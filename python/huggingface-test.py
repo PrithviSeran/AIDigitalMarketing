63e8f3fd2bc5d795293eec9e6b7cdeddcf5a9cf2
prithviseran <prithisha33@gmail.com>
2024-07-05 20:44:54 -0400

    Hugging Face LLM

---
+++ python/huggingface-test.py
@@ -0,0 +1,37 @@
+from huggingface_hub import login
+import transformers
+from transformers import pipeline
+import torch
+
+
+#login(token="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")
+# Load model directly
+#pipe = pipeline("text-generation", model="meta-llama/Meta-Llama-3-8B")
+
+#pipe("Hi How Are you?")
+
+"""
+model_id = "meta-llama/Meta-Llama-3-70B"
+
+#pipeline = transformers.pipeline(
+#    "text-generation", model=model_id, model_kwargs={"torch_dtype": torch.bfloat16}, device_map="auto"
+#)
+
+# Use a pipeline as a high-level helper
+pipe = pipeline("text-generation", model="meta-llama/Meta-Llama-3-70B")
+
+"""
+
+from huggingface_hub import InferenceClient
+
+client = InferenceClient(
+    "meta-llama/Meta-Llama-3-8B-Instruct",
+    token="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",
+)
+
+for message in client.chat_completion(
+	messages=[{"role": "user", "content": "What slang is this?:' Oi Bruv, its chtuesday innit?'"}],
+	max_tokens=500,
+	stream=True,
+):
+    print(message.choices[0].delta.content, end="")
---
+++ python/llama3-test.py
@@ -0,0 +1,22 @@
+import math
+import transformers
+import torch
+import time
+
+ts = time.time()
+lt = time.localtime()
+
+print(f"Loading Model {lt.tm_hour}:{lt.tm_min}:{lt.tm_sec}")
+
+device = "cpu"
+
+model_id = "meta-llama/Meta-Llama-3-8B"
+
+pipeline = transformers.pipeline(
+    "text-generation",
+    model=model_id,
+    model_kwargs={"low_cpu_mem_usage": True}
+#    device_map="auto",
+)
+
+pipeline("how are you?")
