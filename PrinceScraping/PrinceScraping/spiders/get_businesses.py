9bc448b197384c47854b7d7dc1dc6c82081570bc
Prithvi Seran <109385760+PrithviSeran@users.noreply.github.com>
2024-08-21 09:18:39 -0400

    Update get_businesses.py

--- PrinceScraping/PrinceScraping/spiders/get_businesses.py
+++ PrinceScraping/PrinceScraping/spiders/get_businesses.py
@@ -90,7 +90,7 @@ class GetBusinessWebsites(CrawlSpider):
             host ="localhost",
             dbname ="AIDigMar",
             user="postgres",
-            password="*PeterisVal6h7j",
+            password="",
             port="5433")
         
         self.curr = self.conn.cursor()
